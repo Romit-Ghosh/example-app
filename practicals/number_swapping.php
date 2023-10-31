5e974c7e1e3bde25b387b169d05426765ac39f12
Romit <romit1620@gmail.com>
2023-10-31 18:37:40 +0530

    Add Number Swapping code

---
+++ practicals/number_swapping.php
@@ -0,0 +1,18 @@
+<?php
+$number1 = 5;
+$number2 = 10;
+
+echo "Original numbers: ";
+echo "Number 1: " . $number1 . "<br>";
+echo "Number 2: " . $number2 . "<br>";
+
+// Swap the numbers
+$temp = $number1;
+$number1 = $number2;
+$number2 = $temp;
+
+echo "After swapping: ";
+echo "Number 1: " . $number1 . "<br>";
+echo "Number 2: " . $number2 . "<br>";
+?>
+
