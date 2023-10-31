11fdd65f9fbfdae185b34b4d75f004da9b372266
Romit <romit1620@gmail.com>
2023-10-31 18:49:09 +0530

    Add Inverted Triangle code

---
+++ practicals/inverted_triangle.php
@@ -0,0 +1,11 @@
+<?php
+$rows = 5; // Change this to the number of rows you want
+
+for ($i = $rows; $i >= 1; $i--) {
+    for ($j = 1; $j <= $i; $j++) {
+        echo "*";
+    }
+    echo "\n";
+}
+?>
+
