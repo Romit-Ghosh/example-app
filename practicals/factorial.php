a8270c04907a0bba51b87bcbd04c44c3b01d1635
Romit <romit1620@gmail.com>
2023-10-31 18:46:32 +0530

    Add Factorial code

---
+++ practicals/factorial.php
@@ -0,0 +1,15 @@
+<?php
+function factorial($n) {
+    if ($n <= 1) {
+        return 1;
+    } else {
+        return $n * factorial($n - 1);
+    }
+}
+
+$number =6; // Replace this with the number for which you want to calculate the factorial
+$result = factorial($number);
+
+echo "Factorial of $number is $result";
+?>
+
