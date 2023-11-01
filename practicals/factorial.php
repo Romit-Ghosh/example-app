019c3ae0a9a010cba2ad0ef11a500f0ec3d38b85
Romit <romit1620@gmail.com>
2023-11-01 21:48:12 +0530

    Update Factorial of a Number program for readability and maintainability

--- practicals/factorial.php
+++ practicals/factorial.php
@@ -1,4 +1,6 @@
 <?php
+
+//function call to perform factorial
 function factorial($n) {
     if ($n <= 1) {
         return 1;
@@ -8,8 +10,11 @@ function factorial($n) {
 }
 
 $number =6; // Replace this with the number for which you want to calculate the factorial
+
+//calling the function
 $result = factorial($number);
 
+//printing the result
 echo "Factorial of $number is $result";
 ?>
 
