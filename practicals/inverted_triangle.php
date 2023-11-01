dde56f266f3045dbaa9be91888aa76c88f299cdf
Romit <romit1620@gmail.com>
2023-11-01 21:54:19 +0530

    Update Inverted Triangle program for readability and maintainability

--- practicals/inverted_triangle.php
+++ practicals/inverted_triangle.php
@@ -1,6 +1,7 @@
 <?php
 $rows = 5; // Change this to the number of rows you want
 
+// Generating the pattern
 for ($i = $rows; $i >= 1; $i--) {
     for ($j = 1; $j <= $i; $j++) {
         echo "*";
